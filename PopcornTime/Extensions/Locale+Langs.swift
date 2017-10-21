4c6b3c7670b2c9cdf83f55dd211ed8118868535d
Bruno Lemos <brunohplemos@gmail.com>
2017-10-21 20:06:43 -0200

    Added pt-BR subtitles

--- PopcornTime/Extensions/Locale+Langs.swift
+++ PopcornTime/Extensions/Locale+Langs.swift
@@ -6,13 +6,13 @@ extension Locale {
     
     /// Returns a list of common `Locale` ISO language codes.
     static var commonISOLanguageCodes: [String] {
-        return ["en", "fr", "de", "ja", "nl", "it", "es", "da", "fi", "no", "sv", "ko", "zh", "ru", "pl", "pt", "id", "tr", "hu", "el", "ca", "hi", "th", "ms", "cs", "sk", "vi", "ro", "uk", "ar"]
+        return ["en", "fr", "de", "ja", "nl", "it", "es", "da", "fi", "no", "sv", "ko", "zh", "ru", "pl", "pt", "pt-BR", "id", "tr", "hu", "el", "ca", "hi", "th", "ms", "cs", "sk", "vi", "ro", "uk", "ar"]
     }
     
     /// Returns a list of common `Locale` languages.
     static var commonLanguages: [String]  {
         return Locale.commonISOLanguageCodes.flatMap {
-            guard let language = Locale.current.localizedString(forLanguageCode: $0) else { return nil }
+            guard let language = Locale.current.localizedString(forIdentifier: $0) else { return nil }
             return language.localizedCapitalized
         }
     }
